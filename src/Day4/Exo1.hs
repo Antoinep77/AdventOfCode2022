3413a34a01cdf7913751c37f8e55ff8beb729f02
Antoine Paris <antoineparis77@gmail.com>
2022-12-04 09:32:15 +0100

    add day 4

---
+++ src/Day4/Exo1.hs
@@ -0,0 +1,27 @@
+module Day4.Exo1
+  ( solution,
+  )
+where
+
+import Utils
+import qualified Data.Tuple as Tuple
+
+
+solution1 :: String -> Int
+solution1 = length . filter (uncurry isContained) . map ((\[x,y] -> (x,y)) . map ( map read . split "-") . split ",")  . split "\n"
+
+
+solution :: String -> Int
+solution = length . filter (uncurry overlaps) . map ((\[x,y] -> (x,y)) . map ( map read . split "-") . split ",")  . split "\n"
+
+
+isContained :: [Int] -> [Int] -> Bool
+isContained [x1,y1] [x2,y2] = (x1 <= x2 && y1 >= y2) || (x1 >= x2 && y1 <= y2)
+
+overlaps :: [Int] -> [Int] -> Bool
+overlaps [x1,y1] [x2,y2] =  y1 >= x2 && y2 >= x1
+
+
+
+
+
--- src/Main.hs
+++ src/Main.hs
@@ -4,13 +4,13 @@ module Main where
 import System.IO  
 import Control.Monad
 
-import Day3.Exo2
+import Day4.Exo1
 
 
 main :: IO ()
 main = do  
         let list = []
-        handle <- openFile "src/Day3/input.txt" ReadMode
+        handle <- openFile "src/Day4/input.txt" ReadMode
         contents <- hGetContents handle
         let response = solution contents
         print response
